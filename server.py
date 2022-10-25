76b9450a5b5df8acb8c6e9fea12134a9339c3367
rdpcodes <54380958+rdpfeifle@users.noreply.github.com>
2022-10-25 17:06:47 -0600

    Created one route, base.html and homepage with a navbar using Bootstrap

--- server.py
+++ server.py
@@ -7,5 +7,17 @@ import crud
 from jinja2 import StrictUndefined
 
 app = Flask(__name__)
-# app.secret_key = "dev"
+app.secret_key = "dev"
 app.jinja_env.undefined = StrictUndefined
+app.app_context().push()
+
+@app.route("/")
+def homepage():
+    """View homepage."""
+
+    return render_template("homepage.html")
+
+
+if __name__ == "__main__":
+    connect_to_db(app)
+    app.run(host="0.0.0.0", debug=True)
