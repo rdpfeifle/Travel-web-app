1bad309101561079930ae9362f13e91307535e10
rdpcodes <54380958+rdpfeifle@users.noreply.github.com>
2022-10-31 18:58:03 -0600

    Implemented Google maps

--- static/js/maps.js
+++ static/js/maps.js
@@ -1,3 +1,25 @@
+"use strict";
+
 function initMap() {
-  // code goes here
+  const newYorkCoords = {
+    lat: 40.73061,
+    lng: -73.935242,
+  };
+
+  const basicMap = new google.maps.Map(document.querySelector("#map"), {
+    center: newYorkCoords,
+    zoom: 11,
+  });
+
+  const nyMarker = new google.maps.Marker({
+    position: newYorkCoords,
+    title: "New York City",
+    map: basicMap,
+  });
+
+  const nyInfo = new google.maps.InfoWindow({
+    content: "<h1>New York City!</h1>",
+  });
+
+  nyInfo.open(basicMap, nyMarker);
 }
