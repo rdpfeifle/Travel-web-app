6a6447803afcef054d954a81f29ba0d32d5528d9
rdpcodes <54380958+rdpfeifle@users.noreply.github.com>
2023-09-05 18:55:34 -0600

    Change location of ajax call

--- static/js/app.js
+++ static/js/app.js
@@ -20,3 +20,25 @@ flatpickr("input[type=datetime-local]", {
   altInput: true,
   altFormat: "F j, Y",
 });
+
+//---------------------- delete trip ----------------------//
+
+// my trips page
+let deleteTripBtns = document.querySelectorAll(".trip-btn");
+for (const button of deleteTripBtns) {
+  button.addEventListener("click", () => {
+    console.log(button);
+    fetch("/delete-trip", {
+      method: "POST",
+      body: JSON.stringify({ tripToDelete: button.value }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.text())
+      .then((status) => {
+        // button.value is the trip id
+        document.querySelector(`#trip-card-${button.value}`).remove();
+      });
+  });
+}
